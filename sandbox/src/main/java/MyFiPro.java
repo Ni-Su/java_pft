f392284efbe002bb6b04d153986ff265900b4536
Ni-Su <nikolaysukhikh81@gmail.com>
2023-06-01 22:31:59 +0500

    добавлены классы

--- sandbox/src/main/java/MyFiPro.java
+++ sandbox/src/main/java/MyFiPro.java
@@ -4,22 +4,21 @@ public class MyFiPro {
 		hello("all");
 		hello("user");
 
-		double l = 5;
-		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+		Square s = new Square(5);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
 	}
 	public static void hello(String someboby){
 		System.out.println("Hello, " + someboby + "!!!");
 	}
 
-	public static double area(double len){
-		return len * len;
+	public static double area(Square s){
+		return s.l * s.l;
 	}
 
-	public static double area(double a, double b) {
-		return a * b;
+	public static double area(Rectangle r) {
+		return r.a * r.b;
 	}
 }
