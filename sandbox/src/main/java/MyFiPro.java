f13d31ce6584406a27e536fe3d093dc887c6d778
Ni-Su <nikolaysukhikh81@gmail.com>
2023-06-01 21:59:31 +0500

    добавлены несколько функций

--- sandbox/src/main/java/MyFiPro.java
+++ sandbox/src/main/java/MyFiPro.java
@@ -3,10 +3,23 @@ public class MyFiPro {
 	public static void main(String[] args){
 		hello("all");
 		hello("user");
+
+		double l = 5;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 6;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 	}
 	public static void hello(String someboby){
 		System.out.println("Hello, " + someboby + "!!!");
 	}
 
+	public static double area(double len){
+		return len * len;
+	}
 
+	public static double area(double a, double b) {
+		return a * b;
+	}
 }
