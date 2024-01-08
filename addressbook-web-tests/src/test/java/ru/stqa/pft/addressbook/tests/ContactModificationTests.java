c9f081c4c576d28c2b151689a0f31013d40badcb
Ni-Su <nikolaysukhikh81@gmail.com>
2024-01-08 08:45:52 +0500

    5_1 запускаем браузер один раз и выполняем в нём все тесты, внесли изменения в класс ТестБейс

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactModificationTests extends TestBase {
-    @Test
+    @Test(enabled = false)
     public void testContactModification () {
         app.getNavigationHelper().gotoHomePage();
         //int before = app.getGroupHelper().getGroupCount();
