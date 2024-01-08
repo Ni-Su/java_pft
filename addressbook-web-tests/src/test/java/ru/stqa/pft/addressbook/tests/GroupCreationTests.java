958df2be01946e5eb71da08d11483ebfe4be0ec6
Ni-Su <nikolaysukhikh81@gmail.com>
2024-01-08 10:38:35 +0500

    5_4 в ГрупДата внесли изменения и создали сеттеры, удалили конструкторы,вытянули методы в цепочки в тестах, в классе ГрупХелпер сделали шаблон builder

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -13,12 +13,12 @@ public class GroupCreationTests extends TestBase {
     public void testGroupCreation() {
         app.goTo().groupPage();
         List<GroupData> before = app.group().list();
-        GroupData group = new GroupData("test1", "test2", "test3");
+        GroupData group = new GroupData().withName("test2");
         app.group().create(group);
         List<GroupData> after = app.group().list();
         Assert.assertEquals(after.size(), before.size() + 1); //сравниваем размеры списков полученных методом getGroupList()
 
-        group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
+        group.withId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
         before.add(group);
         Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));//из списков делаем множества и сравниваем
 
