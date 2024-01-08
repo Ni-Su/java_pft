14d57614847b699bc781e606313ecc24cd9843d3
Ni-Su <nikolaysukhikh81@gmail.com>
2024-01-08 09:47:57 +0500

    5_3 переименовываем названия методов для улучшения чтения кода

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -6,7 +6,7 @@ import ru.stqa.pft.addressbook.model.ContactData;
 public class ContactModificationTests extends TestBase {
     @Test(enabled = false)
     public void testContactModification () {
-        app.getNavigationHelper().gotoHomePage();
+        app.goTo().gotoHomePage();
         //int before = app.getGroupHelper().getGroupCount();
         app.getContactHelper().initContactModification();
         app.getContactHelper().fillContactForm(new ContactData("test_name", "test_surname", null), false);
